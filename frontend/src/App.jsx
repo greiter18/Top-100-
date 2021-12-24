9a6a8ece1186a8640af92bb30b4f28210b5cd73f
gabriel <gabeh.reiter@gmail.com>
2021-12-23 23:26:25 -0500

    react set up changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [wineList, setWineList] = useState([]);
+
+  const options = {
+    headers: {
+      common: null
+    }
+  };
+  useEffect(() => {
+    getWine().then((res) => {
+      setWineList(res)
+    })
+  }, [])
+
+  const getWine = () => {
+    return axios.get(`https://cors-anywhere.herokuapp.com/https://top-100-example.s3.amazonaws.com/t100_2021_full.json`, 
+    {headers: { 'Content-Type': 'application/json'}})
+  };
+
   return (
     <div>
+      {console.log('---------wine',wineList)}
       <h1>Top 100 </h1>
+      {/* {wineList} */}
     </div>
   );
 }
