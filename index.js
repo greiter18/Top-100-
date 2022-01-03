32430eb7bcbce0167f91ad95c350e7af1bcbafd9
gabriel <gabeh.reiter@gmail.com>
2022-01-03 14:27:48 -0500

    heroku script  updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/key').mongoURI; // our database
+const path = require('path');
 
 mongoose
 .connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
@@ -22,6 +23,13 @@ app.use('/wines', wines)
 app.use('/winery', winery)
 app.use('/tastingnote', tastingnote)
 app.use('/region', region)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server is HOT"));
